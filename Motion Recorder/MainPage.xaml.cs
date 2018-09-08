7603007d5387aabab3ba50fb8ae6db1da75a76f3
文轩 张 <zwx.atomx@outlook.com>
2018-09-08 15:57:22 +0800

    代码备份

--- Motion Recorder/MainPage.xaml.cs
+++ Motion Recorder/MainPage.xaml.cs
@@ -31,79 +31,9 @@ namespace Motion_Recorder
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            StorageFile file = e.Parameter as StorageFile;
-            if (file == null)
-            {
-                ContentFrame.Navigate(typeof(Sensor));
-            }
-            else
-            {
-                ContentFrame.Navigate(typeof(Decoder));
-            }
-
+            sensor.Navigate(typeof(Sensor));
+            decoder.Navigate(typeof(Decoder));
         }
 
-        #region NavView
-
-        private void NavView_Loaded(object sender, RoutedEventArgs e)
-        {
-            // you can also add items in code behind
-            //NavView.MenuItems.Add(new NavigationViewItemSeparator());
-
-            // set the initial SelectedItem
-            foreach (NavigationViewItemBase item in NavView.MenuItems)
-            {
-                if (item is NavigationViewItem && item.Tag.ToString() == "Sensor")
-                {
-                    NavView.SelectedItem = item;
-                    break;
-                }
-            }
-        }
-
-        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            if (args.IsSettingsInvoked)
-            {
-                ContentFrame.Navigate(typeof(About));
-            }
-            else
-            {
-                // find NavigationViewItem with Content that equals InvokedItem
-                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
-                NavView_Navigate(item as NavigationViewItem);
-
-            }
-        }
-
-        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            if (args.IsSettingsSelected)
-            {
-                ContentFrame.Navigate(typeof(About));
-            }
-            else
-            {
-                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
-                NavView_Navigate(item);
-            }
-        }
-
-        private void NavView_Navigate(NavigationViewItem item)
-        {
-            switch (item.Tag)
-            {
-                case "Sensor":
-                    ContentFrame.Navigate(typeof(Sensor));
-                    break;
-
-                case "Decoder":
-                    ContentFrame.Navigate(typeof(Decoder));
-                    break;
-
-            }
-        }
-
-        #endregion
     }
 }
