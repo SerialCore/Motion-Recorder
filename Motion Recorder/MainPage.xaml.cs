7d91f08d6c46dd05b4ec66662264fa83a05fb56a
文轩 张 <zwx.atomx@outlook.com>
2018-11-23 02:33:34 +0800

    解决导航闪退的问题

--- Motion Recorder/MainPage.xaml.cs
+++ Motion Recorder/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using Windows.Devices.Sensors;
 using Windows.Storage;
@@ -27,322 +28,67 @@ namespace Motion_Recorder
         public MainPage()
         {
             this.InitializeComponent();
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-
-            SData = new ObservableCollection<SensorData>();
-            this.DataContext = this;
-
-            accelerationX.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            accelerationY.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            accelerationZ.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            pitchDegrees.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            rollDegrees.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            yawDegrees.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            angularVelocityX.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            angularVelocityY.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
-            angularVelocityZ.AnimationsSpeed = TimeSpan.FromMilliseconds(100);
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        #region NavView
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
         {
-            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Disabled;
+            // you can also add items in code behind
+            //NavView.MenuItems.Add(new NavigationViewItemSeparator());
 
-            _displayRequest.RequestActive();
-            _accelerometer = Accelerometer.GetDefault();
-            _inclinometer = Inclinometer.GetDefault();
-            _gyrometer = Gyrometer.GetDefault();
-            if (_accelerometer != null)
+            // set the initial SelectedItem
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
             {
-                //_isAccelerometerEnable = true;
-                _accelerometer.ReportInterval = 60;
-                _accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
-            }
-            if (_inclinometer != null)
-            {
-                IsInclinometerEnable = true;
-                _inclinometer.ReportInterval = 60;
-                _inclinometer.ReadingChanged += Inclinometer_ReadingChanged;
-            }
-            if (_gyrometer != null)
-            {
-                IsGyrometerEnable = true;
-                _gyrometer.ReportInterval = 60;
-                _gyrometer.ReadingChanged += Gyrometer_ReadingChanged;
-            }
-            if (_accelerometer == null && _inclinometer == null && _gyrometer == null)
-            {
-                record_control.IsEnabled = false;
-                clear_control.IsEnabled = false;
-                store_data.IsEnabled = false;
-            }
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            if (PeriodicTimer != null)
-                PeriodicTimer.Cancel();
-            record_control.Icon = new SymbolIcon(Symbol.Play);
-            record_control.Label = "Record";
-            clear_control.IsEnabled = true;
-            store_data.IsEnabled = true;
-            IsRecording = false;
-
-            _accelerometer.ReadingChanged -= Accelerometer_ReadingChanged;
-            _inclinometer.ReadingChanged -= Inclinometer_ReadingChanged;
-            _gyrometer.ReadingChanged -= Gyrometer_ReadingChanged;
-        }
-
-        DisplayRequest _displayRequest = new DisplayRequest();
-        Accelerometer _accelerometer;
-        Inclinometer _inclinometer;
-        Gyrometer _gyrometer;
-
-        AccelerometerReading areading;
-        InclinometerReading ireading;
-        GyrometerReading greading;
-
-        //bool IsAccelerometerEnable = false;
-        bool IsInclinometerEnable = false;
-        bool IsGyrometerEnable = false;
-
-        int time = 0;
-        bool IsRecording = false;
-        ThreadPoolTimer PeriodicTimer;
-        public ObservableCollection<SensorData> SData { get; set; }
-
-        private void Decoder_Click(object sender, RoutedEventArgs e)
-            => this.Frame.Navigate(typeof(Decoder));
-
-        private void About_Click(object sender, RoutedEventArgs e)
-            => this.Frame.Navigate(typeof(About));
-
-        private void Flyout_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
-            => FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
-
-        #region Control
-
-        private async void Record_Click(object sender, RoutedEventArgs e)
-        {
-            if (!IsRecording)
-            {
-                try
+                if (item is NavigationViewItem && item.Tag.ToString() == "Sensor")
                 {
-                    time = Convert.ToInt32(timeSpan.Text);
+                    NavView.SelectedItem = item;
+                    break;
                 }
-                catch
-                {
-                    await new MessageDialog("Could not identify input").ShowAsync();
-                    return;
-                }
-                record_control.Icon = new SymbolIcon(Symbol.Pause);
-                record_control.Label = "Suspend";
-                clear_control.IsEnabled = false;
-                store_data.IsEnabled = false;
+            }
+        }
 
-                // 既然此按钮能按得动，说明至少有一个传感器能用，一般为加速度传感器
-                if (IsInclinometerEnable && IsGyrometerEnable)
-                    YYY();
-                else if (!IsInclinometerEnable)
-                    YNY();
-                else if (!IsGyrometerEnable)
-                    YYN();
-                else
-                    YNN();
-
-                IsRecording = true;
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(About));
             }
             else
             {
-                if (PeriodicTimer != null)
-                    PeriodicTimer.Cancel();
-                record_control.Icon = new SymbolIcon(Symbol.Play);
-                record_control.Label = "Record";
-                clear_control.IsEnabled = true;
-                store_data.IsEnabled = true;
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavView_Navigate(item as NavigationViewItem);
 
-                IsRecording = false;
             }
         }
 
-        private void Clear_data(object sender, RoutedEventArgs e)
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-            SData.Clear();
-            data_count.Text = "0";
-        }
-
-        private async void Export_table(object sender, RoutedEventArgs e)
-        {
-            FileSavePicker picker = new FileSavePicker();
-            picker.FileTypeChoices.Add("Motion Table", new String[] { ".txt" });
-            picker.SuggestedStartLocation = PickerLocationId.Desktop;
-            picker.SuggestedFileName = "Table-" + DateTime.Now.ToString("yyyyMMddHHmmss");
-            StorageFile file = await picker.PickSaveFileAsync();
-            if (file != null)
+            if (args.IsSettingsSelected)
             {
-                StringBuilder data_record = new StringBuilder();
-                data_record.AppendLine("date\tPeriod\tx-acceleration\ty-acceleration\tz-acceleration\tx-inclination\ty-inclination\tz-inclination\tx-angular-velocity\ty-angular-velocity\tz-angular-velocity");
-                foreach (SensorData data in SData)
-                {
-                    data_record.AppendLine(data.Time + "\t" + data.Span + "\t"
-                        + data.AccelerationX + "\t" + data.AccelerationY + "\t" + data.AccelerationZ + "\t"
-                        + data.PitchDegrees + "\t" + data.RollDegrees + "\t" + data.YawDegrees + "\t"
-                        + data.AngularVelocityX + "\t" + data.AngularVelocityY + "\t" + data.AngularVelocityZ);
-                }
-
-                await FileIO.WriteTextAsync(file, data_record.ToString(), Windows.Storage.Streams.UnicodeEncoding.Utf8);
-
-                data_record.Clear();
+                ContentFrame.Navigate(typeof(About));
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                NavView_Navigate(item);
             }
         }
 
-        private async void Export_object(object sender, RoutedEventArgs e)
+        private void NavView_Navigate(NavigationViewItem item)
         {
-            FileSavePicker picker = new FileSavePicker();
-            picker.FileTypeChoices.Add("Motion Object", new String[] { ".mdo" });
-            picker.SuggestedStartLocation = PickerLocationId.Desktop;
-            picker.SuggestedFileName = "Object-" + DateTime.Now.ToString("yyyyMMddHHmmss");
-            StorageFile file = await picker.PickSaveFileAsync();
-            if (file != null)
+            switch (item.Tag)
             {
-                SensorData[] datas = new SensorData[SData.Count];
-                SData.CopyTo(datas, 0);
+                case "Sensor":
+                    ContentFrame.Navigate(typeof(Sensor));
+                    break;
 
-                string jsonString = JsonConvert.SerializeObject(datas);
-                await FileIO.WriteTextAsync(file, jsonString);
+                case "Decoder":
+                    ContentFrame.Navigate(typeof(Decoder));
+                    break;
+
             }
-        }
-
-        #endregion
-
-        #region Sensor
-
-        private async void Accelerometer_ReadingChanged(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                areading = args.Reading;
-                accelerationX.Value = Math.Round(areading.AccelerationX, 3);
-                accelerationY.Value = Math.Round(areading.AccelerationY, 3);
-                accelerationZ.Value = Math.Round(areading.AccelerationZ, 3);
-            });
-        }
-
-        private async void Inclinometer_ReadingChanged(Inclinometer sender, InclinometerReadingChangedEventArgs args)
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                ireading = args.Reading;
-                pitchDegrees.Value = Math.Round(ireading.PitchDegrees, 3);
-                rollDegrees.Value = Math.Round(ireading.RollDegrees, 3);
-                yawDegrees.Value = Math.Round(ireading.YawDegrees, 3);
-            });
-        }
-
-        private async void Gyrometer_ReadingChanged(Gyrometer sender, GyrometerReadingChangedEventArgs args)
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                greading = args.Reading;
-                angularVelocityX.Value = Math.Round(greading.AngularVelocityX, 3);
-                angularVelocityY.Value = Math.Round(greading.AngularVelocityY, 3);
-                angularVelocityZ.Value = Math.Round(greading.AngularVelocityZ, 3);
-            });
-        }
-
-        private void YYY()
-        {
-            TimeSpan period = TimeSpan.FromMilliseconds(time);
-            PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
-            {
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    SData.Add(new SensorData
-                    {
-                        Time = areading.Timestamp,
-                        Span = time,
-                        AccelerationX = areading.AccelerationX.ToString("F2"),
-                        AccelerationY = areading.AccelerationY.ToString("F2"),
-                        AccelerationZ = areading.AccelerationZ.ToString("F2"),
-                        PitchDegrees = ireading.PitchDegrees.ToString("F2"),
-                        RollDegrees = ireading.RollDegrees.ToString("F2"),
-                        YawDegrees = ireading.YawDegrees.ToString("F2"),
-                        AngularVelocityX = greading.AngularVelocityX.ToString("F2"),
-                        AngularVelocityY = greading.AngularVelocityY.ToString("F2"),
-                        AngularVelocityZ = greading.AngularVelocityZ.ToString("F2")
-                    });
-
-                    data_count.Text = SData.Count.ToString();
-                });
-            }, period);
-        }
-
-        private void YYN()
-        {
-            TimeSpan period = TimeSpan.FromMilliseconds(time);
-            PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
-            {
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    SData.Add(new SensorData
-                    {
-                        Time = areading.Timestamp,
-                        Span = time,
-                        AccelerationX = areading.AccelerationX.ToString("F2"),
-                        AccelerationY = areading.AccelerationY.ToString("F2"),
-                        AccelerationZ = areading.AccelerationZ.ToString("F2"),
-                        PitchDegrees = ireading.PitchDegrees.ToString("F2"),
-                        RollDegrees = ireading.RollDegrees.ToString("F2"),
-                        YawDegrees = ireading.YawDegrees.ToString("F2")
-                    });
-
-                    data_count.Text = SData.Count.ToString();
-                });
-            }, period);
-        }
-
-        private void YNY()
-        {
-            TimeSpan period = TimeSpan.FromMilliseconds(time);
-            PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
-            {
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    SData.Add(new SensorData
-                    {
-                        Time = areading.Timestamp,
-                        Span = time,
-                        AccelerationX = areading.AccelerationX.ToString("F2"),
-                        AccelerationY = areading.AccelerationY.ToString("F2"),
-                        AccelerationZ = areading.AccelerationZ.ToString("F2"),
-                        AngularVelocityX = greading.AngularVelocityX.ToString("F2"),
-                        AngularVelocityY = greading.AngularVelocityY.ToString("F2"),
-                        AngularVelocityZ = greading.AngularVelocityZ.ToString("F2")
-                    });
-
-                    data_count.Text = SData.Count.ToString();
-                });
-            }, period);
-        }
-
-        private void YNN()
-        {
-            TimeSpan period = TimeSpan.FromMilliseconds(time);
-            PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
-            {
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    SData.Add(new SensorData
-                    {
-                        Time = areading.Timestamp,
-                        Span = time,
-                        AccelerationX = areading.AccelerationX.ToString("F2"),
-                        AccelerationY = areading.AccelerationY.ToString("F2"),
-                        AccelerationZ = areading.AccelerationZ.ToString("F2")
-                    });
-
-                    data_count.Text = SData.Count.ToString();
-                });
-            }, period);
         }
 
         #endregion
